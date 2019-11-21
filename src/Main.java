2c8f27ca3f0b94187ff49df97bd10376b86850a0
inna-s <i.selezko@gmail.com>
2019-11-21 14:40:42 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main  {
+    public static void main(String[] args) {
+        int [] numbers = {54, 31, 12, 76, 89, 80, 51, 13};
+       
+        int i= 0;
+
+        for (i=0; i<numbers.length; i++);
+            for(int j = 0 ; j < i ; j++){
+        if (numbers[i]>numbers[i+1]);
+               j = numbers[i];
+               numbers[i]=numbers[i+1];
+               numbers[i+1]=j;
+        }
+
+
+    }}
